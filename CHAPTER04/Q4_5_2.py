c381af82221c66a2470f2a11c8a9a0d2693541f4
tamashirosouta <n23019@std.it-college.ac.jp>
2023-07-25 07:22:14 +0000

    updata

--- CHAPTER04/Q4_5_2.py
+++ CHAPTER04/Q4_5_2.py
@@ -1,9 +1,13 @@
-s_coordi_list = ["1.0.2.2.3.5", "2.1.3.2.5.5", "1.2.1.3.2.2", "2.1.3.1.4.5"]
+s_coordi_list = ["1.0,2.2,3.5", "2.1,3.2,5.5", "1.2,1.3,2.2", "2.1,3.1,4.5"]
 
-def parse_coord(coord_str):
-    return list(map(float, coord_str.split('.')))
+def str_to_float_coordi (s_coordi):  
+    p = s_coordi.split(",") 
+    return list (map (float, p))
 
-f_coordi_list = list(map(parse_coord, s_coordi_list))
+def str_to_float_coordi_iter(s_coordi_list):
+    return map(str_to_float_coordi, s_coordi_list) 
 
-print(f_coordi_list)
+f_coordi_list=list(str_to_float_coordi_iter(s_coordi_list))
+
+print (f_coordi_list)
 
