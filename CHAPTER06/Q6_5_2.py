1e5c0442ce9e45b01e1ecf5f46631456d8fb8c20
tamashirosouta <n23019@std.it-college.ac.jp>
2023-08-23 06:47:27 +0000

    create my_math2.py and update

--- CHAPTER06/Q6_5_2.py
+++ CHAPTER06/Q6_5_2.py
@@ -1,5 +1,6 @@
+import my_math2
 x, y, exp = 2, 5, 32
-ans = my_pow(x, y)
+ans = my_math2.my_pow(x, y)
 print("Test my_pow({},{}) -> {}, exp: {} ---- ".format(x, y, ans, exp), end = '')
 if ans == exp:
     print("Test OK")
